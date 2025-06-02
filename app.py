0beabfde027269e25978f8a4e0fb93feb4698a24
Phillip Hatland Rødseth <113134107+vtolvr@users.noreply.github.com>
2025-06-02 17:41:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return redirect("https://ddns.volary.cloud"),301
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def redirect_all(path):
+    return redirect("https://ddns.volary.cloud", code=301)
 
 if __name__ == '__main__':
   app.run(debug=False, port=3000, host='0.0.0.0')
